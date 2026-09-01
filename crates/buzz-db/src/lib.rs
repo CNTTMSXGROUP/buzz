abbf98052ffcd98a19fd7bbce018586e7b133fa6
John Tennant <jtennant@squareup.com>
2026-09-01 16:09:32 -0400

    Allow Project admins to manage related channels
    Signed-off-by: John Tennant <jtennant@squareup.com>

--- crates/buzz-db/src/lib.rs
+++ crates/buzz-db/src/lib.rs
@@ -54,7 +54,8 @@ pub use community::{
 pub use error::{DbError, Result};
 pub use event::{EventQuery, DEFAULT_MAX_PAGE_LIMIT};
 pub use project_state::{
-    ProjectLifecycleApplyResult, ProjectLifecycleStatus, ProjectStateProjectionCandidate,
+    ProjectChangeApplyResult, ProjectLifecycleApplyResult, ProjectLifecycleStatus,
+    ProjectRelatedChannelChange, ProjectStateProjectionCandidate,
     ProjectStateProjectionCommitResult,
 };
 pub use reaction::ReactionEventInsertOutcome;
